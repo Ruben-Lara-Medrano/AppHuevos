6898fd3f1790753bdbf409bf4985bed443ceab1a
AlvaroOrtiz <alorpes@gmail.com>
2020-11-28 21:14:53 +0100

    Modificado el Main (Solo diseño) Revisar el diseno_btn.xml ya que no funciona bien como dice el TODO en el Main

--- app/src/main/java/com/example/apphuevos/MainActivity.java
+++ app/src/main/java/com/example/apphuevos/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Todo en drawable - diseno_btn no funciona bien del todo ya que no pilla el color aqua que se encuentra en colors
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
